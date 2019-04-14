f9528152d0cf35e480d2261bc4597d35aad7c792
Luan Banh <49538471+luanbanh@users.noreply.github.com>
2019-04-14 16:47:07 -0400

    Static graphics

---
+++ top.vhd
@@ -0,0 +1,99 @@
+library IEEE; use IEEE.std_logic_1164.all; 
+use IEEE.numeric_std.all; 
+
+entity top is 
+    port ( 
+	HSYNC : out std_logic; 
+	VSYNC : out std_logic; 
+	RGB : out std_logic_vector(5 downto 0) 
+); 
+end entity;
+
+architecture synth of top is
+component HSOSC is
+    generic ( 
+    	CLKHF_DIV : String := "0b00"); -- Clock divider, see documentation for details 
+    port( 
+		CLKHFPU : in std_logic := 'X'; 
+		CLKHFEN : in std_logic := 'X'; 
+		CLKHF : out std_logic := 'X'
+	); 
+end component; 
+
+component pllgen is 
+    port( 
+		outglobal_o: out std_logic; 
+		outcore_o: out std_logic; 
+		ref_clk_i: in std_logic; 
+		rst_n_i: in std_logic
+	); 
+end component;
+
+component vga is
+	port ( 
+		inputclk : in std_logic;
+		valid: out std_logic; 
+		out_row : out unsigned(9 downto 0);
+		out_col :  out unsigned(9 downto 0);
+		hsync : out std_logic;
+		vsync : out std_logic
+	); 
+end component;
+
+component pattern_gen2 is
+	port (
+		clk: in std_logic;
+		valid : in std_logic;
+		row : in unsigned(9 downto 0);
+		col : in unsigned(9 downto 0);
+		rgb : out std_logic_vector(5 downto 0)
+    );
+end component;
+
+signal inputclk: std_logic;
+signal pixclk: std_logic;
+signal pixvalid: std_logic;
+signal pixrow: unsigned(9 downto 0);
+signal pixcol: unsigned(9 downto 0);
+signal OUTPUTWAVE: std_logic;
+
+begin 
+    -- Instantiate the 48 MHz source clock 
+    clkgen : HSOSC 
+	port map (
+	CLKHFPU => '1', -- Power up, 
+	CLKHFEN => '1', -- Enable output 
+	CLKHF => inputclk
+    ); 
+
+    -- Create a 25.175 MHz pixel clock 
+    pll : pllgen 
+    port map( 
+	outglobal_o=> pixclk,
+	outcore_o=> OUTPUTWAVE, 
+	ref_clk_i=> inputclk, 
+	rst_n_i=> '1' 
+    );
+	
+    -- Connect the VGA driver
+    vgadriver : vga 
+    port map ( 
+	inputclk => pixclk,
+	valid => pixvalid,
+	out_row => pixrow, 
+	out_col => pixcol, 
+	hsync => HSYNC,
+	vsync => VSYNC
+    );
+
+   -- Connnect the pattern generator
+   pattern : pattern_gen2
+   port map (
+	clk => pixclk,
+	valid => pixvalid,
+	row => pixrow,
+	col => pixcol,
+	rgb => RGB
+   ); 
+   
+end;
---
+++ vga.vhd
@@ -0,0 +1,48 @@
+library IEEE; use IEEE.std_logic_1164.all; 
+use IEEE.numeric_std.all; 
+
+entity vga is
+    port ( 
+	inputclk : in std_logic;
+	valid: out std_logic; 
+	out_row : out unsigned(9 downto 0);
+	out_col : out unsigned(9 downto 0);
+	hsync : out std_logic;
+	vsync : out std_logic
+); 
+end entity;
+
+architecture synth of vga is
+    signal row : unsigned(9 downto 0) := to_unsigned(0,10);
+    signal col : unsigned(9 downto 0) := to_unsigned(0,10);
+	constant COL_PIX : integer := 800;
+	constant ROW_LIN : integer := 525;
+	constant VISIBLE_PIX : integer := 640;
+	constant VISIBLE_LIN : integer := 480;
+
+begin
+    process (inputclk) is
+	begin
+        if rising_edge(inputclk) then
+		-- increment col every clock cycle
+			col <= col + to_unsigned(1,10);
+
+		-- increment row after 800 pixel
+			if (col > COL_PIX) then
+				row <= row + to_unsigned(1,10);
+				col <= to_unsigned(0,10);
+			end if;
+
+		-- reset row when needed
+			if (row > ROW_LIN) then
+				row <= to_unsigned(0,10);
+			end if;
+		end if;
+    end process;
+
+    valid <= '1' when (col <= to_unsigned(VISIBLE_PIX,10) and row <= to_unsigned(VISIBLE_LIN,10)) else '0';
+    hsync <= '1' when (col < to_unsigned(656,10) or col >= to_unsigned(752,10)) else '0';
+    vsync <= '1' when (row < to_unsigned(490,10) or row >= to_unsigned(492,10)) else '0';
+    out_row <= row;
+    out_col <= col;
+end;
