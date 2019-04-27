bb5e25e7bbb562d77ddf21c6ba60094e2540e8fb
Caroline V <Caroline.Vanderlee@tufts.edu>
2019-04-27 16:17:11 -0400

    collision check integration commented out

--- game_graphics.vhd
+++ game_graphics.vhd
@@ -44,15 +44,6 @@ component alien_graphics is
     rd_clk_i: in std_logic);
 end component;
 
---component random_num_gen is 
-  --  port (
-    --    clk : in std_logic; 
-      --  reset : in std_logic;
-        --enable : in std_logic;  
-        --count : out std_logic_vector(4 downto 0)
-    --); 
---end component; 
-
 -- Colors
 constant RED: std_logic_vector(5 downto 0) := "110000";
 constant GREEN: std_logic_vector(5 downto 0) := "001100";
@@ -77,7 +68,6 @@ constant UPDATE_ROW: integer := 500;
 constant STEP: integer := 4;
 -- Distance bullet travels
 constant VELOCITY: integer := 6;
-
 -- Commands from the controller
 constant LEFT_CMD: integer := 1;
 constant RIGHT_CMD: integer := 2;
@@ -106,22 +96,21 @@ constant START_X: integer := 304;
 signal ship_x: integer;
 signal ship_location: integer;
 
--- ship bullet location
+--signal ship_lives : integer range 0 to 3 := 3; --added for collisions
+
+-- bullet location
 signal ship_bullet_x: integer;
 signal ship_bullet_y: integer;
 signal bullet_location: integer;
+--signal bullet_on : std_logic; --added for collisions
 
 -- alien location
 signal alien_x : integer;
 signal alien_y : integer;
 signal reverse : std_logic := '0';
-
--- alien bullet location 
-signal alien_bullet_x : integer;
-signal alien_bullet_y : integer; 
-signal alien_bullet_loc_x : integer; 
-signal alien_bullet_loc_y : integer; 
-signal alien_bullet_start : std_logic := '0'; 
+signal counter : unsigned(25 downto 0);
+signal count: integer;
+--signal alien_on : std_logic <= '1'; --added for collisions
 
 -- check if valid
 signal rom_valid: std_logic;
@@ -130,13 +119,24 @@ signal ship_on: std_logic;
 signal alien_on: std_logic;
 signal ship_lb, ship_rb: std_logic;
 signal bullet_on: std_logic;
-signal alien_bullet_on: std_logic; 
 
 -- output 32 bits word from rom
 signal read_data: std_logic_vector(31 downto 0);
-signal alien_read_data: std_logic_vector(31 downto 0);
-signal rom_on:  std_logic_vector(31 downto 0);
-signal alien_rom_on: std_logic_vector(31 downto 0);
+signal alien_read_data: std_logic_vector(31 downto 0);	
+
+--component collision_checker is
+  --port (
+	--clk : in std_logic;
+    --bullet_on : in std_logic;
+    --bullet_x : in integer;
+    --bullet_y : in integer;
+    --target_on : in std_logic;
+    --target_x : in integer;
+	--target_y : in integer;
+    --target_off : out std_logic;
+    --lives : out integer range 0 to 3
+  --);
+--end component;
 
 begin
 	ship: spaceship_graphics port map(
@@ -164,35 +164,63 @@ begin
     wr_en_i=> '0', 
 	rd_clk_i=> clk
 	);
+	
+	--collide: collision_checker port map (
+	  --clk => clk;
+      --bullet_on => bullet_on;
+      --bullet_x => ship_bullet_x;
+      --bullet_y => ship_bullet_y;
+      --target_on => alien_on;
+      --target_x => alien_x;
+	  --target_y => alien_y;
+      --target_off => alien_on;
+      --lives => ship_lives
+	--);
 
-process (clk, cmd, ship_location, bullet_location, alien_x, alien_y, alien_bullet_loc_x, alien_bullet_loc_y) is begin
-	-- Update ship when in the dead zone and some inputs occurs
+process (clk, cmd, ship_location, bullet_location) is begin
+	-- Update memories when in the dead zone and some inputs occurs
 	if rising_edge(clk) and row = to_unsigned(UPDATE_ROW,10) and col = to_unsigned(660,10) then
-		if cmd = START_CMD then
-			ship_location <= START_X;						-- Make ship starts at the middle
-		elsif cmd = LEFT_CMD and ship_lb = '1' then
-			ship_location <= ship_location - STEP;			-- Change ship's location when receives input
+		-- Change ship's location when receives input
+		if cmd = LEFT_CMD and ship_lb = '1' then
+			ship_location <= ship_location - STEP;
 		elsif cmd = RIGHT_CMD and ship_rb = '1' then
 			ship_location <= ship_location + STEP;
+		-- Make ship starts at the middle
+		elsif cmd = START_CMD then
+			ship_location <= START_X;
 		end if;
+	-- Ship stay in one place if no inputs
 	elsif cmd = STANDBY_CMD then
-		ship_location <= ship_location;						-- Ship stay in one place if no inputs
+		ship_location <= ship_location;
 	end if;
 	
-	-- Update bullet when in the dead zone and some inputs occurs
+	-- Shoot
 	if rising_edge(clk) and row = to_unsigned(UPDATE_ROW,10) and col = to_unsigned(670,10) then
-		if cmd = A_CMD and bullet_location <= 0 then
+		if cmd = A_CMD and bullet_location <= 0 then --bullet starts
+			--bullet_on <= '1'; --added for collisions
 			bullet_location <= SHIP_TOP_B - 16;
 			ship_bullet_x <= ship_location;
-		elsif bullet_location > 0 then
+		elsif bullet_location > 0 then --bullet moves
+			--bullet_on <= '1'; --added for collisions
 			bullet_location <= bullet_location - VELOCITY;
 			ship_bullet_x <= ship_bullet_x;
-		elsif bullet_location <= 0 then
+		elsif bullet_location <= 0 then --bullet hides
+			--bullet_on <= '0'; --added for collisions
 			bullet_location <= -16;
 		end if;
 	end if;
 	
+	--if rising_edge(clk) then
+		--counter <= counter + to_unsigned(1,26);
+		--if counter(25) = '1' and count < alien_x + 320 then
+			--count <= count + 48;
+			--alien_x <= alien_x + count;
+		--elsif count >= 320 then
+			--count <= 0;
+		--end if;
+	--end if;
 	if rising_edge(clk) and row = to_unsigned(UPDATE_ROW,10) and col = to_unsigned(680,10) then
+		--a; --added for collisions
 		if cmd = START_CMD then
 			alien_y <= ALIEN_TOP_B;
 			alien_x <= ALIEN_L_B + 80;
@@ -205,37 +233,13 @@ process (clk, cmd, ship_location, bullet_location, alien_x, alien_y, alien_bulle
 		alien_x <= alien_x;
 		alien_y <= alien_y;
 	end if;
-	
-	if rising_edge(clk) and row = to_unsigned(UPDATE_ROW,10) and col = to_unsigned(670,10) then
-		if cmd = A_CMD and alien_bullet_loc_x <= 0 then
-			alien_bullet_loc_x <= ALIEN_BOT_B - 16;
-			alien_bullet_x <= alien_x;
-		elsif alien_bullet_loc_x > 0 then
-			alien_bullet_loc_x <= alien_bullet_loc_x - VELOCITY;
-			alien_bullet_x <= alien_bullet_x;
-		elsif alien_bullet_loc_x <= 0 then
-			alien_bullet_loc_x <= -16;
-		end if;
-	end if;
-	
-	if rising_edge(clk) then
-		rom_on <= read_data;
-		rom_x <= x_coord when rom_valid ='1' else 0;
-		
-		alien_rom_on <= alien_read_data;
-		alien_rom_x <= alien_x_coord when alien_rom_valid ='1' else 0;
-		
-	end if;
 end process;
 
 -- Give ship_x the ship's stored location
 ship_x <= ship_location;
 
--- Give bullet_y the ship's bullet stored location
+-- Give bullet_y the ship's bullet stored loction
 ship_bullet_y <= bullet_location;
-
--- Give alien_bullet_x and alien_bullet_y the alien's bullet stored location
-alien_bullet_y <= alien_bullet_loc_y; 
 
 -- Get the x and y coordinates for ship rom
 y_coord <= to_integer(row) - SHIP_TOP_B;
@@ -247,6 +251,7 @@ rom_valid <= '1' when (y_coord >= 0 and y_coord < 32) and
 
 -- Update rom_x and rom_y if it is inside the ROM image
 rom_y <= std_logic_vector(to_unsigned(y_coord,5)) when rom_valid ='1' else "00000";
+rom_x <= x_coord when rom_valid ='1' else 0;
 	
 -- Valid size for the ship
 ship_on <= '1' when row >= to_unsigned(SHIP_TOP_B,10) and row <= to_unsigned(SHIP_BOT_B,10)and 
@@ -259,13 +264,9 @@ ship_rb <= '1' when ship_x+32 < SHIP_R_B else '0';
 -- Valid size of the ship's bullet
 bullet_on <= '1' when row >= to_unsigned(ship_bullet_y,10) and row <= to_unsigned(ship_bullet_y + 8,10)and 
 				  col >= to_unsigned(ship_bullet_x + 15,10) and col <= to_unsigned(ship_bullet_x + 17,10) else '0';
-				 
--- Valid size of the alien's bullet
-alien_bullet_on <= '1' when row >= to_unsigned(alien_bullet_y,10) and row <= to_unsigned(alien_bullet_y + 8,10)and 
-				  col >= to_unsigned(alien_bullet_x + 15,10) and col <= to_unsigned(alien_bullet_x + 17,10) else '0';
 
 -- Get the x and y coordinates for alien rom
-alien_y_coord <= (to_integer(row) - alien_y)mod(32);
+alien_y_coord <= to_integer(row) - alien_y;
 alien_x_coord <= (to_integer(col) - alien_x)mod(32);
 
 reverse <= '1' when alien_x + 320= ALIEN_R_B else '0' when alien_x = ALIEN_L_B;
@@ -275,19 +276,15 @@ alien_rom_valid <= '1' when (alien_y_coord >= 0 and alien_y_coord < 32) and
 				(alien_x_coord >= 0 and alien_x_coord < 32) else '0';
 
 -- Update alien_rom_x and alien_rom_y if it is inside the ROM image
---alien_rom_y <= std_logic_vector(to_unsigned(alien_y_coord,5)) when alien_rom_valid ='1' else "00000";
---alien_rom_x <= alien_x_coord when alien_rom_valid ='1' else 0;
-
--- Update alien_rom_x and alien_rom_y if it is inside the ROM image
 alien_rom_y <= std_logic_vector(to_unsigned(alien_y_coord,5)) when alien_rom_valid ='1' else "00000";
+alien_rom_x <= alien_x_coord when alien_rom_valid ='1' else 0;
 
 -- Valid size for the alien
-alien_on <= '1' when row >= to_unsigned(alien_y, 10) and row <= to_unsigned(alien_y +96,10)and 
+alien_on <= '1' when row >= to_unsigned(alien_y, 10) and row <= to_unsigned(alien_y +32,10)and 
 				  col >= to_unsigned(alien_x,10) and col <= to_unsigned(alien_x + 320,10) else '0';
 				  
 -- Output color
-rgb <= GREEN when valid ='1' and ship_on= '1' and rom_on(rom_x)= '1' else 
+rgb <= GREEN when valid ='1' and ship_on= '1' and read_data(rom_x)= '1' else 
 	   WHITE when valid ='1' and bullet_on = '1' else
-	   WHITE when valid = '1' and alien_bullet_on = '1' else
-	   RED when valid = '1' and alien_on= '1' and alien_rom_on(alien_rom_x)= '1' else "000000";
+	   RED when valid = '1' and alien_on= '1' and alien_read_data(alien_rom_x)= '1' else "000000";
 end;
